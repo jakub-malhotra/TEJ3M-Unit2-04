eec2f16aa3a5aa5239ac95ef2cdfb007263867bc
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-10-03 11:08:22 -0400

    format

--- main.ino
+++ main.ino
@@ -1,91 +1,52 @@
 /*
-
   Created by: Jakub Malhotra
-
   Created on: October 2023
 
-
-
   Turns an LED on for one second, then off for one second, repeatedly.
-
 */
 
 
-
-void setup()
-
-{
-
-  pinMode(11, OUTPUT);
-
-  pinMode(12, OUTPUT);
-
-  pinMode(13, OUTPUT);
-
+void setup() {
+    pinMode(11, OUTPUT);
+    pinMode(12, OUTPUT);
+    pinMode(13, OUTPUT);
 }
 
+void loop() {
+    digitalWrite(11, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(13, LOW);
 
+    delay(1000); // Wait for 1000 millisecond(s)
 
-void loop()
+    digitalWrite(11, LOW);
+    digitalWrite(12, HIGH);
+    digitalWrite(13, LOW);
 
-{
+    delay(1000); // Wait for 1000 millisecond(s)
 
-  digitalWrite(13, HIGH);
+    digitalWrite(11, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(13, HIGH);
 
-  digitalWrite(12, LOW);
+    delay(1000); // Wait for 1000 millisecond(s)
 
-  digitalWrite(11, LOW);
+    digitalWrite(11, HIGH);
+    digitalWrite(12, HIGH);
+    digitalWrite(13, LOW);
 
-  delay(1000); // Wait for 1000 millisecond(s)
+    delay(1000); // Wait for 1000 millisecond(s)
 
-  digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(12, HIGH);
+    digitalWrite(13, HIGH);
 
-  digitalWrite(11, HIGH);
+    delay(1000); // Wait for 1000 millisecond(s)
 
-  digitalWrite(13, LOW);
+    digitalWrite(11, HIGH);
+    digitalWrite(12, HIGH);
+    digitalWrite(13, HIGH);
 
-  delay(1000); // Wait for 1000 millisecond(s)
-
-  digitalWrite(11, LOW);
-
-  digitalWrite(13, LOW);
-
-  digitalWrite(12, HIGH);
-
-  delay(1000); // Wait for 1000 millisecond(s)
-
-  
-
-  digitalWrite(11, HIGH);
-
-  digitalWrite(13, HIGH);
-
-  digitalWrite(12, LOW);
-
-  delay(1000); // Wait for 1000 millisecond(s)
-
-  digitalWrite(13, LOW);
-
-  digitalWrite(12, HIGH);
-
-  digitalWrite(11, HIGH);
-
-  delay(1000); // Wait for 1000 millisecond(s)
-  
-  digitalWrite(11, LOW);
-  
-  digitalWrite(12, HIGH);
-  
-  digitalWrite(13, HIGH);
-  
-  delay(1000); // Wait for 1000 millisecond(s)
-
-  digitalWrite(11, HIGH);
-
-  digitalWrite(12, HIGH);
-
-  digitalWrite(13, HIGH);
-
-  delay(1000); // Wait for 1000 millisecond(s)
+    delay(1000); // Wait for 1000 millisecond(s)
 
 }
